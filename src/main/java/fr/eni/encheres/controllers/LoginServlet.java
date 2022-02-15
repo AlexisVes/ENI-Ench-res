fab46ec78c976963e519e180a82760597fa6947f
Baptiste Rolland <baptiste.rolland2021@campus-eni.fr>
2022-02-15 10:42:24 +0100

    correctif controllers

--- src/main/java/fr/eni/encheres/controllers/LoginServlet.java
+++ src/main/java/fr/eni/encheres/controllers/LoginServlet.java
@@ -15,7 +15,7 @@ import fr.eni.encheres.bll.UserManager;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/Login")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
