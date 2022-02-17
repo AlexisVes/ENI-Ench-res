bebe2123bcb9032a2c99923ec357694f4d0d1bf6
Nicolas BRETECHER <nicolas.bretecher@gmail.com>
2022-02-17 16:43:38 +0100

    test

---
+++ src/main/java/fr/eni/encheres/dal/jdbc/RetraitDAOJdbcImpl.java
@@ -0,0 +1,24 @@
+package fr.eni.encheres.dal.jdbc;
+
+import fr.eni.encheres.bo.Retrait;
+import fr.eni.encheres.dal.DALException;
+import fr.eni.encheres.dal.RetraitDAO;
+
+public class RetraitDAOJdbcImpl implements RetraitDAO{
+
+	@Override
+	/**
+	 * Une méthode permettant d'insérer en base de données le lieu du retrait 
+	 */
+	public void insertRetrait(Retrait retrait) throws DALException {
+	
+		
+	}
+
+	@Override
+	public Retrait getRetrait() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
