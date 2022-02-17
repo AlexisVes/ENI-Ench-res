1ff23f4b3dc967a3c83de90ffc27a0a089ced148
Alexis Veslin <veslinalexis44@gmail.com>
2022-02-17 14:41:02 +0100

    ihm

--- src/main/java/fr/eni/encheres/controllers/SellArticleServlet.java
+++ src/main/java/fr/eni/encheres/controllers/SellArticleServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.encheres.controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,14 @@ public class SellArticleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//Remonter cette liste vers l'IHM qui va afficher les articles disponibles aux utilisateurs
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/connect/sell_article.jsp");
+			
+		if( rd != null)
+		{
+			rd.forward(request, response);
+		}
 	}
 
 	/**
