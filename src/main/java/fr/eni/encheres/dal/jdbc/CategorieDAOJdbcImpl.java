0ff6855b6be21e955551e1ddb33c75fa859c718d
Nicolas BRETECHER <nicolas.bretecher@gmail.com>
2022-02-17 15:20:43 +0100

    maj select categorie etc...

---
+++ src/main/java/fr/eni/encheres/dal/jdbc/CategorieDAOJdbcImpl.java
@@ -0,0 +1,45 @@
+package fr.eni.encheres.dal.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.encheres.bo.Categorie;
+import fr.eni.encheres.dal.CategorieDAO;
+import fr.eni.encheres.dal.DALException;
+
+public class CategorieDAOJdbcImpl implements CategorieDAO{
+	
+	private static final String SELECT_CATEGORIE = "SELECT libelle FROM CATEGORIES;";
+
+	@Override
+	public List<Categorie> getCategories() throws DALException {
+
+		//On établit la connexion vers la BDD
+		List<Categorie> listeCategorie = new ArrayList<Categorie>();
+		Connection cnx = null;
+		
+		cnx = ConnectionProvider.getConnection();		
+		Statement rqt;
+		
+		try {
+			rqt = cnx.createStatement();
+			ResultSet rs = rqt.executeQuery(SELECT_CATEGORIE);
+			
+			while(rs.next()) {
+				Categorie categorieCourant = new Categorie(rs.getString("libelle"));
+				listeCategorie.add(categorieCourant);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return listeCategorie;
+	}
+
+}
