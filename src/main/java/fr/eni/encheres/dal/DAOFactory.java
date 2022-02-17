985af7aa2cec973e0b25bf39dc6768649235d550
Nicolas BRETECHER <nicolas.bretecher@gmail.com>
2022-02-17 15:28:19 +0100

    update dao factory

--- src/main/java/fr/eni/encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/encheres/dal/DAOFactory.java
@@ -2,6 +2,7 @@ package fr.eni.encheres.dal;
 
 
 import fr.eni.encheres.dal.jdbc.ArticleDAOJdbcImpl;
+import fr.eni.encheres.dal.jdbc.CategorieDAOJdbcImpl;
 import fr.eni.encheres.dal.jdbc.UserDAOJdbcImpl;
 import fr.eni.encheres.dal.nosql.UserDAONoSQLImpl;
 
@@ -23,6 +24,17 @@ public class DAOFactory {
 				return new ArticleDAOJdbcImpl();
 			case "NOSQL" :
 				return new ArticleDAOJdbcImpl();
+			default : 
+				throw new DALException("Source de données inconnue");
+		}
+	}
+	
+	public static CategorieDAO createCategorieDAO(String type) throws DALException {
+		switch(type) {
+			case "JDBC":
+				return new CategorieDAOJdbcImpl();
+			case "NOSQL" :
+				return new CategorieDAOJdbcImpl();
 			default : 
 				throw new DALException("Source de données inconnue");
 		}
