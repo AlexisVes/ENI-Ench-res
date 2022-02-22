19ad3f340698bbca3109a2e0f988b960c2417dbd
Alexis Veslin <veslinalexis44@gmail.com>
2022-02-22 12:45:19 +0100

    Merge branch 'main' of https://github.com/AlexisVes/ENI-Ench-res.git into main

--- src/main/java/fr/eni/encheres/bll/EnchereManager.java
+++ src/main/java/fr/eni/encheres/bll/EnchereManager.java
@@ -57,20 +57,12 @@ public class EnchereManager
 				}
 			
 			}
-<<<<<<< HEAD
-		}
-		else
-		{
-			try {
-				System.out.println("crédit : " +  userDAO.getUserById(no_utilisateur).getCredit());
-				if (prixSaisi > article.getPrixVente() && userDAO.getUserById(no_utilisateur).getCredit() >= prixSaisi) {
-=======
+
 			else
 			{
 	
 				if (prixSaisi > article.getPrixVente() && userDAO.getUserById(no_utilisateur).getCredit() >= prixSaisi) 
 				{
->>>>>>> branch 'main' of https://github.com/AlexisVes/ENI-Ench-res.git
 					enchere = new Enchere( now, prixSaisi, article.getNoArticle(),  no_utilisateur );
 					enchereDAO.insertEnchere(enchere);	
 				}
