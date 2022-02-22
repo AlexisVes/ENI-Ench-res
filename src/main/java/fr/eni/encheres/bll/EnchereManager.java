06f741cac7eaa52f3ef08f506c3f48eae2903f20
Alexis Veslin <veslinalexis44@gmail.com>
2022-02-22 11:11:04 +0100

    Correction indentation commentaire controllers

--- src/main/java/fr/eni/encheres/bll/EnchereManager.java
+++ src/main/java/fr/eni/encheres/bll/EnchereManager.java
@@ -53,6 +53,11 @@ public class EnchereManager {
 		}
 		
 	}
+	
+	public void insertEnchere( Enchere enchere )
+	{
+		enchereDAO.insertEnchere(enchere);
+	}
 
 	public synchronized static EnchereManager getInstance()
 {
